4bb88dbe3843a2ba9f49e81302727ed9bb8f6b58
Tae-hyeon Kim <simoltobella@gmail.com>
2023-08-02 02:19:34 +0900

    Add: 2-2
    타입스크립트 설정 파일

---
+++ ch2/2-2.ts
@@ -0,0 +1,7 @@
+// 2-1. 타입스크립트 설정 파일
+
+// 설명: 타입스크립트 설정 파일을 작성함으로써 타임 검증에 필요한 옵션들을 커스터마이징할 수 있습니다.
+//  tsconfig.json에 작성된 옵션에 따라 컴파일링 진행됩니다.
+
+// 참고:
+//  - TS Compiler Options: https://www.typescriptlang.org/docs/handbook/compiler-options.html
