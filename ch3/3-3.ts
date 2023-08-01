cc6825a9ef5d5e863c955c8c2991c05f7e9c6624
Tae-hyeon Kim <simoltobella@gmail.com>
2023-08-02 05:53:14 +0900

    Add: 3-3
    함수 타입 - 파라미터, 반환값 선언법

---
+++ ch3/3-3.ts
@@ -0,0 +1,11 @@
+// 3-3. 함수 타입 - 파라미터, 반환값 선언
+
+// 예제1-1: TS 함수 파라미터
+function sum(a: number, b: number) {
+  return a + b
+}
+
+// 예제1-2: TS 함수 반환값
+function add(a: number, b: number): number {
+  return a + b
+}
