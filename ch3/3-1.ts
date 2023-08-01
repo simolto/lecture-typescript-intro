5d18bd85f56d643e3d08d4091294c2a078fb53b6
Tae-hyeon Kim <simoltobella@gmail.com>
2023-08-02 03:34:11 +0900

    Add: 3-1
    기본 타입 - 문자, 숫자, 배열 선언법

---
+++ ch3/3-1.ts
@@ -0,0 +1,11 @@
+// 3-1. 기본 타입 - 문자, 숫자, 배열 선언
+
+// 예제1-1: TS 문자형
+const text: string = 'hi'
+
+// 예제1-2: TS 숫자형
+const num: number = 1
+
+// 예제1-3: TS 배열(2가지 방식)
+const arr: Array<number> = [1, 2, 3]
+const arr2: number[] = [4, 5, 6]
