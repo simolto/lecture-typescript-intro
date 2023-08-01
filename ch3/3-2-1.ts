0bbc1b733fc27b0407aacf35cd72c0110d13a132
Tae-hyeon Kim <simoltobella@gmail.com>
2023-08-02 04:23:47 +0900

    Add: 3-2-1
    기본 타입 - 튜플, 객체, 진위값 선언법

---
+++ ch3/3-2-1.ts
@@ -0,0 +1,11 @@
+// 3-2-1. 기본 타입 - 튜플, 객체, 진위값 선언
+
+// 예제1-1: TS 튜플
+const arr: [string, number, boolean] = ['1', 1, true]
+
+// 예제1-2: TS 객체
+const obj: object = { name: 'ellie', age: 21 }
+const person: { name: string; age: number } = { name: 'bob', age: 31 }
+
+// 예제1-3: TS 진위값
+const show: boolean = true
