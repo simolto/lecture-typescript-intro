c112274424e9daa425040cf926183fd35470dc85
Tae-hyeon Kim <simoltobella@gmail.com>
2023-08-07 07:27:19 +0900

    Add: ch9-2
    자바스크립트 프로토타입 이해(소개)

---
+++ ch9/9-2.ts
@@ -0,0 +1,7 @@
+// 9-2. 클래스 - 자바스크립트 프로토타입 이해(소개)
+
+// 설명: 클래스가 나온 배경에는 자바스크립트 프로토타입의 상속과도 연결됩니다.
+//  프로토타입 체이닝을 통해서 기존 객체의 속성과 함수는 유지한 채로 체이닝을 통해 가져올 객체의 속성과 함수까지도 사용(활용) 가능해지는 효과가 있습니다.
+
+// 참고:
+//  - JS Prototype: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
